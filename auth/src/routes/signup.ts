d2081c992dbe95dc952310fc789ca42ea2b818b6
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-09 18:49:43 +0000

    async error handling

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,7 @@
 import { DatabaseConnectionError } from "./../errors/database-connection-error";
 import { RequestValidationError } from "./../errors/request-validation-error";
 import express, { Request, Response } from "express";
+import "express-async-errors";
 import { body, validationResult } from "express-validator";
 
 const router = express.Router();
@@ -14,7 +15,7 @@ router.post(
       .isLength({ min: 4, max: 20 })
       .withMessage("Password must be between 4 and 20 characters"),
   ],
-  (req: Request, res: Response) => {
+  async (req: Request, res: Response) => {
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
