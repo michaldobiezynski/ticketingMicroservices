af38aba04066116d51dd4752ac640bfcf4d0dc76
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-14 17:55:31 +0000

    easy auth solution

--- auth/src/routes/__test__/current-user.test.ts
+++ auth/src/routes/__test__/current-user.test.ts
@@ -3,7 +3,7 @@ import request from "supertest";
 import { app } from "../../app";
 
 it("it responds with details about the current user", async () => {
-  await request(app)
+  const authResponse = await request(app)
     .post("/api/users/signup")
     .send({
       email: "test@test.com",
@@ -11,12 +11,13 @@ it("it responds with details about the current user", async () => {
     })
     .expect(201);
 
+  const cookie = authResponse.get("Set-Cookie");
+
   const response = await request(app)
     .get("/api/users/currentuser")
+    .set("Cookie", cookie)
     .send()
     .expect(200);
 
-  console.log(response.body);
-
-  expect(response.get("Set-Cookie")).toBeDefined();
+  expect(response.body.currentUser.email).toEqual("test@test.com");
 });
