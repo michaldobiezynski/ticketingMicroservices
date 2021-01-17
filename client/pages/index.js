57c58ecc917cc77452af4db06e71b62192873e1d
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-17 19:23:37 +0000

    specifying the host for the auth service

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,22 +1,28 @@
 import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
-  // console.log(currentUser);
+  console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-  // const response = await axios.get("/api/users/currentuser");
-
-  // return response.data;
-
   if (typeof window === "undefined") {
     // on the server
+    const { data } = await axios.get(
+      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
+      {
+        headers: {
+          Host: "ticketing.dev",
+        },
+      }
+    );
+
+    return data;
   } else {
     // on the browser
+    const { data } = await axios.get("/api/users/currentuser");
+    return data;
   }
-
-  return {};
 };
 
 export default LandingPage;
