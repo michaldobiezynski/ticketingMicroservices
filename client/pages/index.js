1c85171f4d674d99cadbe1ac55e3c5239a3f0bad
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-17 19:39:32 +0000

    a reusable api client

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,26 +1,14 @@
-import axios from "axios";
+import buildClient from "../api/build-client";
 
 const LandingPage = ({ currentUser }) => {
   console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({ req }) => {
-  if (typeof window === "undefined") {
-    // on the server
-    const { data } = await axios.get(
-      "http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser",
-      {
-        headers: req.headers,
-      }
-    );
-
-    return data;
-  } else {
-    // on the browser
-    const { data } = await axios.get("/api/users/currentuser");
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const { data } = await client.get("/api/users/currentuser");
+  return data;
 };
 
 export default LandingPage;
