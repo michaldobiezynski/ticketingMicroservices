455558f2dc4e2128a4417f01f105a748772e0b07
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-17 19:01:53 +0000

    on the server or the browser

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,14 +1,22 @@
 import axios from "axios";
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
+  // console.log(currentUser);
   return <h1>Landing Page</h1>;
 };
 
 LandingPage.getInitialProps = async () => {
-  const response = await axios.get("/api/users/currentuser");
+  // const response = await axios.get("/api/users/currentuser");
 
-  return response.data;
+  // return response.data;
+
+  if (typeof window === "undefined") {
+    // on the server
+  } else {
+    // on the browser
+  }
+
+  return {};
 };
 
 export default LandingPage;
