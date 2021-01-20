a85666755ff2435757067ae1731fc41cf22e5f0a
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-20 21:21:25 +0000

    creating the router in the ticket service

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,8 @@ import cookieSession from "cookie-session";
 
 import { errorHandler, NotFoundError } from "@michaldobiezynski_tickets/common";
 
+import { createTicketRouter } from "./routes/new";
+
 const app = express();
 app.set("trust proxy", true);
 app.use(json());
@@ -14,6 +16,8 @@ app.use(
     secure: process.env.NODE_ENV !== "test",
   })
 );
+
+app.use(createTicketRouter);
 
 app.all("*", async (req, res, next) => {
   throw new NotFoundError();
