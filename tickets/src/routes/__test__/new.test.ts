7d6aa6006a360d0bd68c9e20698aace32921546e
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-21 19:25:12 +0000

    testing request validation

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -16,6 +16,32 @@ it("return a status other than 401 if the user is signed in", async () => {
     .send({});
   expect(response.status).not.toEqual(401);
 });
-it("returns an error if an invalid title is provided", async () => {});
-it("returns an error is an invalid price is provided", async () => {});
+it("returns an error if an invalid title is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "",
+      price: 10,
+    })
+    .expect(400);
+});
+it("returns an error is an invalid price is provided", async () => {
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "fgsdfsd",
+      price: -10,
+    })
+    .expect(400);
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({
+      title: "adssdfdsc",
+    })
+    .expect(400);
+});
 it("creates a ticket with valid inputs", async () => {});
