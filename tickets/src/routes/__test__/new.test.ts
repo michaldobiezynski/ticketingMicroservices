62cbeb955662989679b0a0a21e2da01a93758fa8
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-21 19:20:32 +0000

    building a session

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -10,7 +10,10 @@ it("can only be accessed if the user is signed in", async () => {
   await request(app).post("/api/tickets").send({}).expect(401);
 });
 it("return a status other than 401 if the user is signed in", async () => {
-  const response = await request(app).post("/api/tickets").send({});
+  const response = await request(app)
+    .post("/api/tickets")
+    .set("Cookie", global.signin())
+    .send({});
   expect(response.status).not.toEqual(401);
 });
 it("returns an error if an invalid title is provided", async () => {});
--- tickets/src/test/setup.ts
+++ tickets/src/test/setup.ts
@@ -1,13 +1,14 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
 import request from "supertest";
+import jwt from "jsonwebtoken";
 
 import { app } from "../app";
 
 declare global {
   namespace NodeJS {
     interface Global {
-      signin(): Promise<string[]>;
+      signin(): string[];
     }
   }
 }
@@ -38,21 +39,25 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signin = async () => {
+global.signin = () => {
   // Build a JWT payload. {id, email}
-
+  const payload = {
+    id: "1fasfdasdad",
+    email: "test@test.com",
+  };
 
   // Create the JWT!
-
+  const token = jwt.sign(payload, process.env.JWT_KEY!);
 
   // Build session Object. {jwt: MY_JWT}
-
+  const session = { jwt: token };
 
   // Turn that session into JSON
-
+  const sessionJSON = JSON.stringify(session);
 
   // Take JSON and encode it as base64
-
+  const base64 = Buffer.from(sessionJSON).toString("base64");
 
   // return a string thats the cookie with the encoded data
+  return [`express:sess=${base64}`];
 };
