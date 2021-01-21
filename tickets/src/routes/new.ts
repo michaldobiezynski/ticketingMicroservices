28c2479875403434ebcf708d2b560e864be16b19
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-21 20:10:35 +0000

    ticket creation via route handler and updating ticket creation test

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from "express";
 import { requireAuth } from "@michaldobiezynski_tickets/common";
 import { body } from "express-validator";
 
+import { Ticket } from "../models/tickets";
 import {
   validateRequest,
   BadRequestError,
@@ -19,8 +20,18 @@ router.post(
       .withMessage("Price must be greated than 0."),
   ],
   validateRequest,
-  (req: Request, res: Response) => {
-    res.sendStatus(200);
+  async (req: Request, res: Response) => {
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
