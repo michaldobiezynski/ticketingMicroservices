04d9234ceddc5732a74148fccc5a1b60c3a0eca7
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-01-30 18:30:20 +0000

    subtle service coupling - validating ticket id in orders service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -1,9 +1,27 @@
+import { mongoose } from "mongoose";
+import { body } from "express-validator";
+import {
+  requireAuth,
+  validateRequest,
+} from "@michaldobiezynski_tickets/common";
 import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-router.post("/api/orders", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders",
+  requireAuth,
+  [
+    body("ticketId")
+      .not()
+      .isEmpty()
+      .custom((input: string) => mongoose.Types.ObjectId.isValid(input))
+      .withMessage("Ticket ID is required"),
+  ],
+  validateRequest,
+  async (req: Request, res: Response) => {
+    res.send({});
+  }
+);
 
 export { router as newOrderRouter };
