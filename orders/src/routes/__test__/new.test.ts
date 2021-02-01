6233dfc7302ccea1d439d7cef387d4c8f91a45c7
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-01 09:00:53 +0000

    a slightly complicated test to test fetching orders with tickets

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -22,15 +22,15 @@ it("returns an error if the ticket is already reserved", async () => {
   await ticket.save();
 
   const order = Order.build({
-      ticket,
-      userId: 'ahdshahxn'
-      status: OrderStatus.Created,
-      expiresAt: new Date();
+    ticket,
+    userId: "ahdshahxn",
+    status: OrderStatus.Created,
+    expiresAt: new Date(),
   });
 
   await order.save();
 
-     await request(app)
+  await request(app)
     .post("/api/orders")
     .set("Cookie", global.signin())
     .send({ ticketId: ticket.id })
@@ -38,18 +38,18 @@ it("returns an error if the ticket is already reserved", async () => {
 });
 
 it("reserves a ticket", async () => {
-    const ticket = Ticket.build({
-        title: 'concert',
-        price: 20
-    })
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
 
-    await ticket.save();
+  await ticket.save();
 
-    await request(app)
+  await request(app)
     .post("/api/orders")
     .set("Cookie", global.signin())
     .send({ ticketId: ticket.id })
     .expect(201);
 });
 
-it.todo('emits an order created event');
+it.todo("emits an order created event");
