952d92f715c50f4f78fb978b1d8845b4d09439d6
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-02 20:39:23 +0000

    testing event publishing

--- orders/src/routes/__test__/delete.test.ts
+++ orders/src/routes/__test__/delete.test.ts
@@ -3,6 +3,7 @@ import { app } from "../../app";
 import mongoose from "mongoose";
 import { Order, OrderStatus } from "../../models/order";
 import { Ticket } from "../../models/ticket";
+import { natsWrapper } from "../../nats-wrapper";
 
 it("marks an order as cancelled", async () => {
   const ticket = Ticket.build({
@@ -34,4 +35,27 @@ it("marks an order as cancelled", async () => {
 
   expect(fetchedOrder.status).toEqual(OrderStatus.Cancelled);
 });
-it.todo("emits a order cancelled event");
+it("emits a order cancelled event", async () => {
+  const ticket = Ticket.build({
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  const userOne = global.signin();
+
+  const { body: order } = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userOne)
+    .send({ ticketId: ticket.id })
+    .expect(201);
+
+  await request(app)
+    .delete(`/api/orders/${order.id}`)
+    .set("Cookie", userOne)
+    .send()
+    .expect(204);
+
+  expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
