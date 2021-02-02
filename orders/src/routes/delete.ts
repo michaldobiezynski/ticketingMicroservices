b4978416c02887ec5027744e8d5cf73b937b0d8e
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-02 18:12:42 +0000

    cancelling an order

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,9 +1,32 @@
 import express, { Request, Response } from "express";
-
+import {
+  NotFoundError,
+  requireAuth,
+  NotAuthorisedError,
+} from "@michaldobiezynski_tickets/common";
+import { Ticket } from "../models/ticket";
+import { Order, OrderStatus } from "../models/order";
 const router = express.Router();
 
-router.delete("/api/orders/:orderId", async (req: Request, res: Response) => {
-  res.send({});
-});
+router.post(
+  "/api/orders/:orderId",
+  requireAuth,
+  async (req: Request, res: Response) => {
+    const { orderId } = req.params;
+    const order = await Order.findById(orderId);
+
+    if (!order) {
+      throw new NotFoundError();
+    }
+
+    if (order.userId !== req.currentUser!.id) {
+      throw new NotAuthorisedError();
+    }
+    order.status = OrderStatus.Cancelled;
+    await order.save();
+
+    res.send(order);
+  }
+);
 
 export { router as deleteOrderRouter };
