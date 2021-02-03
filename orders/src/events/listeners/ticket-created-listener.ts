ac936f803de0afa6a9e351a32d0fa4b8e49db851
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-03 19:54:34 +0000

    dceclare queueGroupName in a separate file to avoid typos

--- orders/src/events/listeners/ticket-created-listener.ts
+++ orders/src/events/listeners/ticket-created-listener.ts
@@ -5,11 +5,10 @@ import {
 } from "@michaldobiezynski_tickets/common";
 import { Message } from "node-nats-streaming";
 import { Ticket } from "../../models/ticket";
+import { queueGroupName } from "./queue-group-name";
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = "orders-service";
-  onMessage(data: TicketCreatedEvent["data"], msg: Message) {
-      
-  }
+  queueGroupName = queueGroupName;
+  onMessage(data: TicketCreatedEvent["data"], msg: Message) {}
 }
