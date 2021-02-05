1bde606ad5b6bd531476cf57f9d280e045f79071
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-05 22:44:02 +0000

    including versions in events

--- common/src/events/order-created-event.ts
+++ common/src/events/order-created-event.ts
@@ -5,6 +5,7 @@ export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
+    version: number;
     status: OrderStatus.Created;
     userID: string;
     expiresAt: string;
