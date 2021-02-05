182748b69085648eb643fd35e540f80868df2d07
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-05 23:07:34 +0000

    applyign a version query

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -1,3 +1,4 @@
+import { updateIfCurrentPlugin } from "mongoose-update-if-current";
 import mongoose from "mongoose";
 import { Order, OrderStatus } from "./order";
 
@@ -10,6 +11,7 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
+  version: number;
   isReserved(): Promise<boolean>;
 }
 
@@ -38,6 +40,9 @@ const ticketSchema = new mongoose.Schema(
     },
   }
 );
+
+ticketSchema.set("versionKey", "version");
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 ticketSchema.statics.build = (attrs: TicketAttrs) => {
   return new Ticket({
