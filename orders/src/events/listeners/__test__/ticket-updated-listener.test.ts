026c3864f4e55bed6791e3ad6ce95eb0268e6a96
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-06 11:21:11 +0000

    testing the ticket updated listener

---
+++ orders/src/events/listeners/__test__/ticket-updated-listener.test.ts
@@ -0,0 +1,61 @@
+import { TicketUpdatedListener } from "../ticket-updated-listener";
+import { natsWrapper } from "../../../nats-wrapper";
+import { TicketUpdatedEvent } from "@michaldobiezynski_tickets/common";
+import mongoose from "mongoose";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../../models/ticket";
+
+const setup = async () => {
+  // create an instance of the listener
+  const listener = new TicketUpdatedListener(natsWrapper.client);
+
+  // create and save a ticket
+  const ticket = Ticket.build({
+    id: new mongoose.Types.ObjectId().toHexString(),
+    title: "concert",
+    price: 20,
+  });
+
+  await ticket.save();
+
+  // create a fake data object
+  const data: TicketUpdatedEvent["data"] = {
+    version: ticket.version + 1,
+    id: ticket.id,
+    title: "new gig",
+    price: 99,
+    userId: new mongoose.Types.ObjectId().toHexString(),
+  };
+
+  // create a fake message object
+  // @ts-ignore
+  const msg: Message = {
+    ack: jest.fn(),
+  };
+
+  return { listener, data, msg, ticket };
+};
+
+it("finds, updates, and saves a ticket", async () => {
+  const { listener, data, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure a ticket was Updated!
+  const ticket = await Ticket.findById(data.id);
+
+  expect(ticket).toBeDefined();
+  expect(ticket!.title).toEqual(data.title);
+  expect(ticket!.price).toEqual(data.price);
+});
+
+it("acks the message", async () => {
+  const { data, listener, msg } = await setup();
+
+  // call the onMessage function with the data object + message object
+  await listener.onMessage(data, msg);
+
+  // write assertions to make sure ack function was called
+  expect(msg.ack).toHaveBeenCalled();
+});
