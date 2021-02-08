f77e5340cd6c7c2cbefa7677843ea26085fb0426
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-08 11:44:45 +0000

    reserving a ticket

--- tickets/src/events/listeners/order-created-listener.ts
+++ tickets/src/events/listeners/order-created-listener.ts
@@ -5,11 +5,27 @@ import {
   OrderCreatedEvent,
   Subjects,
 } from "@michaldobiezynski_tickets/common";
+import { Ticket } from "../../models/tickets";
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  async onMessage(data: OrderCreatedEvent, msg: Message) {
-      
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    // Find the ticket that the order is reserving
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    // If not ticket, throw error
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    // Mark the ticket as being reserved by setting its orderId proprty
+    ticket.set({ orderId: data.id });
+
+    // Save the ticket
+    await ticket.save();
+
+    // ack the message
+    msg.ack();
   }
 }
