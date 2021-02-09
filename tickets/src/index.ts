62a66694581905b490add32c4aaf3e17f87f696f
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-09 19:47:33 +0000

    dont forget to listen

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,3 +1,5 @@
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 import { natsWrapper } from "./nats-wrapper";
 import mongoose from "mongoose";
 
@@ -33,6 +35,9 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCreatedListener(natsWrapper.client).listen();
+    new OrderCancelledListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
