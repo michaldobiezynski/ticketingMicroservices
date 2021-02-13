7fe451a76b3b93cd0d25dcbc55bdf382c6f3af10
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-13 20:57:33 +0000

    testing order validation before payment

---
+++ payments/src/routes/__test__/new.test.ts
@@ -0,0 +1,36 @@
+import request from "supertest";
+import mongoose from "mongoose";
+import { app } from "../../app";
+import { Order } from "../../models/order";
+import { OrderStatus } from "@michaldobiezynski_tickets/common";
+
+it("returns a 404 when purchasing an order that does not exist", async () => {
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "sahhad",
+      orderId: mongoose.Types.ObjectId().toHexString(),
+    })
+    .expect(404);
+});
+it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {
+  const order = Order.build({
+    id: mongoose.Types.ObjectId().toHexString(),
+    userId: mongoose.Types.ObjectId().toHexString(),
+    version: 0,
+    price: 20,
+    status: OrderStatus.Created,
+  });
+  await order.save();
+
+  await request(app)
+    .post("/api/payments")
+    .set("Cookie", global.signin())
+    .send({
+      token: "sahhad",
+      orderId: order.id,
+    })
+    .expect(401);
+});
+it("returns a 400 when purchasing an cancelled order", async () => {});
