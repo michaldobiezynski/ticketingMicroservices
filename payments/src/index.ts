45a5e894ce6c870bac999fef19117053b2bceb3a
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-13 19:46:57 +0000

    starting the listeners

--- payments/src/index.ts
+++ payments/src/index.ts
@@ -1,5 +1,7 @@
 import { natsWrapper } from "./nats-wrapper";
 import mongoose from "mongoose";
+import { OrderCancelledListener } from "./events/listeners/order-cancelled-listener";
+import { OrderCreatedListener } from "./events/listeners/order-created-listener";
 
 import { app } from "./app";
 
@@ -33,6 +35,9 @@ const start = async () => {
 
     process.on("SIGINT", () => natsWrapper.client.close());
     process.on("SIGTERM", () => natsWrapper.client.close());
+
+    new OrderCancelledListener(natsWrapper.client).listen();
+    new OrderCreatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
