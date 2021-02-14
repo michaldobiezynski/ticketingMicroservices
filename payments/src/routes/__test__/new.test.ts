c725e35b2de09987e570b6fd6ae238cc61c842c5
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-14 14:00:40 +0000

    defining new event type - payment created

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -17,46 +17,46 @@ it("returns a 404 when purchasing an order that does not exist", async () => {
     .expect(404);
 });
 
-it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {
-  const order = Order.build({
-    id: mongoose.Types.ObjectId().toHexString(),
-    userId: mongoose.Types.ObjectId().toHexString(),
-    version: 0,
-    price: 20,
-    status: OrderStatus.Created,
-  });
-  await order.save();
+// it("returns a 401 when purchasing an order that doesnt belong to the user", async () => {
+//   const order = Order.build({
+//     id: mongoose.Types.ObjectId().toHexString(),
+//     userId: mongoose.Types.ObjectId().toHexString(),
+//     version: 0,
+//     price: 20,
+//     status: OrderStatus.Created,
+//   });
+//   await order.save();
 
-  await request(app)
-    .post("/api/payments")
-    .set("Cookie", global.signin())
-    .send({
-      token: "asldkfj",
-      orderId: order.id,
-    })
-    .expect(401);
-});
+//   await request(app)
+//     .post("/api/payments")
+//     .set("Cookie", global.signin())
+//     .send({
+//       token: "asldkfj",
+//       orderId: order.id,
+//     })
+//     .expect(401);
+// });
 
-it("returns a 400 when purchasing a cancelled order", async () => {
-  const userId = mongoose.Types.ObjectId().toHexString();
-  const order = Order.build({
-    id: mongoose.Types.ObjectId().toHexString(),
-    userId,
-    version: 0,
-    price: 20,
-    status: OrderStatus.Cancelled,
-  });
-  await order.save();
+// it("returns a 400 when purchasing a cancelled order", async () => {
+//   const userId = mongoose.Types.ObjectId().toHexString();
+//   const order = Order.build({
+//     id: mongoose.Types.ObjectId().toHexString(),
+//     userId,
+//     version: 0,
+//     price: 20,
+//     status: OrderStatus.Cancelled,
+//   });
+//   await order.save();
 
-  await request(app)
-    .post("/api/payments")
-    .set("Cookie", global.signin(userId))
-    .send({
-      orderId: order.id,
-      token: "asdlkfj",
-    })
-    .expect(400);
-});
+//   await request(app)
+//     .post("/api/payments")
+//     .set("Cookie", global.signin(userId))
+//     .send({
+//       orderId: order.id,
+//       token: "asdlkfj",
+//     })
+//     .expect(400);
+// });
 
 // it("returns a 201 with valid inputs", async () => {
 //   const userId = mongoose.Types.ObjectId().toHexString();
