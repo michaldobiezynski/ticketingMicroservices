7221326fae293d51168173985b1fe96f13aedd87
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-14 11:38:20 +0000

    mocked stripe client

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import { app } from "../../app";
 import { Order } from "../../models/order";
 import { OrderStatus } from "@michaldobiezynski_tickets/common";
+import { stripe } from "../../stripe";
 
 jest.mock("../../stripe.ts");
 
@@ -57,7 +58,7 @@ it("returns a 400 when purchasing an cancelled order", async () => {
     .expect(400);
 });
 
-it("returns a 204 wiht valid inputs", async () => {
+it("returns a 201 wiht valid inputs", async () => {
   const userId = mongoose.Types.ObjectId().toHexString();
 
   const order = Order.build({
@@ -76,5 +77,10 @@ it("returns a 204 wiht valid inputs", async () => {
       token: "tok_visa",
       orderId: order.id,
     })
-    .expect(204);
+    .expect(201);
+
+  const chargeOptions = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+  expect(chargeOptions.source).toEqual("tok_visa");
+  expect(chargeOptions.amount).toEqual(20 * 100);
+  expect(chargeOptions.currency).toEqual("usd");
 });
