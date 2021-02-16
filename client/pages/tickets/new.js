4e8198352cddf6f0641428b3b9aab36a26ee19aa
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-16 11:00:37 +0000

    sanitizing price input - adding state handlers for title and price

--- client/pages/tickets/new.js
+++ client/pages/tickets/new.js
@@ -1,15 +1,44 @@
+import { useState } from "react";
+
 const NewTicket = () => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+
+    if (isNaN(value)) {
+      return;
+    }
+    setPrice(value.toFixed(2));
+  };
+
   return (
     <div>
       <h1>Create a Ticket</h1>
       <form action="">
         <div className="form-group">
           <label htmlFor="">Title</label>
-          <input className="form-control" type="text" />
+          <input
+            value={title}
+            onChange={(e) => {
+              setTitle(e.target.value);
+            }}
+            className="form-control"
+            type="text"
+          />
         </div>
         <div className="form-group">
           <label htmlFor="">Price</label>
-          <input className="form-control" type="text" />
+          <input
+            onBlur={onBlur}
+            value={price}
+            onChange={(e) => {
+              setPrice(e.target.value);
+            }}
+            className="form-control"
+            type="text"
+          />
         </div>
         <button
           className="btn btn-primary
