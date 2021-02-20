1b06fd2ce3978ef59731bbea5a366a7c6369c36f
michaldobiezynski <michaldobiezynski@hotmail.com>
2021-02-20 15:54:38 +0000

    small changes to test automated deployment of few services

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -9,7 +9,7 @@ import mongoose from "mongoose";
 import { app } from "./app";
 
 const start = async () => {
-  console.log("Starting....");
+  console.log("Starting....hmm...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
